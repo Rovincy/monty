e5b169760850399d9285446451afadd732ef7471
Rovincy <72986365+Rovincy@users.noreply.github.com>
2023-12-18 02:49:04 +0000

    Latest Commit

--- main.c
+++ main.c
@@ -1,54 +1,45 @@
-#include <stdio.h>
-#include <string.h>
 #include "monty.h"
-
-#define BUFFER_SIZE 256 // Adjust as needed for expected line length
-
-/* Global bus structure definition */
-bus_t bus = { NULL, NULL, NULL, 0 };
-
+bus_t bus = {NULL, NULL, NULL, 0};
 /**
- * main - monty code interpreter
- * @argc: Number of arguments
- * @argv: Monty file location
- * Return: 0 on success
- */
-int main(int argc, char *argv[]) {
-  // Check for correct number of arguments
-  if (argc != 2) {
-    fprintf(stderr, "USAGE: monty file\n");
-    exit(EXIT_FAILURE);
-  }
+* main - monty code interpreter
+* @argc: number of arguments
+* @argv: monty file location
+* Return: 0 on success
+*/
+int main(int argc, char *argv[])
+{
+	char *content;
+	FILE *file;
+	size_t size = 0;
+	ssize_t read_line = 1;
+	stack_t *stack = NULL;
+	unsigned int counter = 0;
 
-  // Open Monty file
-  FILE *file = fopen(argv[1], "r");
-  if (!file) {
-    fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-    exit(EXIT_FAILURE);
-  }
-  bus.file = file; // Associate file with bus structure
-
-  // Initialize variables
-  char buffer[BUFFER_SIZE]; // Buffer to store each line
-  unsigned int counter = 0; // Line counter
-  stack_t *stack = NULL; // Stack head pointer
-
-  // Loop through each line in the file
-  while (fgets(buffer, BUFFER_SIZE, file) != NULL) {
-    // Remove newline character if present
-    size_t len = strchr(buffer, '\n') - buffer;
-    if (len > 0 && buffer[len - 1] == '\n') {
-      buffer[len - 1] = '\0';
-    }
-
-    // Process line number and execute command
-    counter++;
-    execute(buffer, &stack, counter, file); // Call your `execute` function
-  }
-
-  // Clean up resources
-  free_stack(stack); // Free allocated memory for stack
-  fclose(file); // Close the Monty file
-
-  return 0; // Exit successfully
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	bus.file = file;
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (read_line > 0)
+	{
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		bus.content = content;
+		counter++;
+		if (read_line > 0)
+		{
+			execute(content, &stack, counter, file);
+		}
+		free(content);
+	}
+	free_stack(stack);
+	fclose(file);
+return (0);
 }
