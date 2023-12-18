cec66fa7f7583b0a164124ed905cee2004e12bbe
Rovincy <72986365+Rovincy@users.noreply.github.com>
2023-12-18 02:42:26 +0000

    Update main.c

--- main.c
+++ main.c
@@ -2,46 +2,52 @@
 #include <string.h>
 #include "monty.h"
 
-#define BUFFER_SIZE 256
+#define BUFFER_SIZE 256 // Adjust as needed for expected line length
 
-bus_t bus = {NULL, NULL, NULL, 0};
+bus_t bus = {NULL, NULL, NULL, 0}; // Global bus structure
 
 /**
  * main - monty code interpreter
- * @argc: number of arguments
- * @argv: monty file location
+ * @argc: Number of arguments
+ * @argv: Monty file location
  * Return: 0 on success
  */
 int main(int argc, char *argv[]) {
-  char *content;
-  FILE *file;
-  size_t size = 0;
-  unsigned int counter = 0;
-  stack_t *stack = NULL;
-
+  // Check for correct number of arguments
   if (argc != 2) {
     fprintf(stderr, "USAGE: monty file\n");
     exit(EXIT_FAILURE);
   }
 
-  file = fopen(argv[1], "r");
-  bus.file = file;
+  // Open Monty file
+  FILE *file = fopen(argv[1], "r");
   if (!file) {
     fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
     exit(EXIT_FAILURE);
   }
+  bus.file = file; // Associate file with bus structure
 
-  while ((content = fgets(bus.buffer, BUFFER_SIZE, file)) != NULL) {
-    size_t len = strchr(bus.buffer, '\n') - bus.buffer;
-    if (len > 0 && bus.buffer[len - 1] == '\n') {
-      bus.buffer[len - 1] = '\0'; // remove newline character
+  // Initialize variables
+  char buffer[BUFFER_SIZE]; // Buffer to store each line
+  unsigned int counter = 0; // Line counter
+  stack_t *stack = NULL; // Stack head pointer
+
+  // Loop through each line in the file
+  while (fgets(buffer, BUFFER_SIZE, file) != NULL) {
+    // Remove newline character if present
+    size_t len = strchr(buffer, '\n') - buffer;
+    if (len > 0 && buffer[len - 1] == '\n') {
+      buffer[len - 1] = '\0';
     }
+
+    // Process line number and execute command
     counter++;
-    execute(bus.buffer, &stack, counter, file);
+    execute(buffer, &stack, counter, file); // Call your `execute` function
   }
 
-  free_stack(stack);
-  fclose(file);
+  // Clean up resources
+  free_stack(stack); // Free allocated memory for stack
+  fclose(file); // Close the Monty file
 
-  return 0;
+  return 0; // Exit successfully
 }
