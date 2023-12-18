eea8ce0fa3d283a153d0dd08d70c1873f39e8377
Rovincy <72986365+Rovincy@users.noreply.github.com>
2023-12-18 01:43:11 +0000

    Update main.c

--- main.c
+++ main.c
@@ -33,13 +33,8 @@ int main(int argc, char *argv[])
 	while (read_line > 0)
 	{
 		content = NULL;
-		read_line = getline(&content, &size, file);
 		bus.content = content;
 		counter++;
-		if (read_line > 0)
-		{
-			execute(content, &stack, counter, file);
-		}
 		free(content);
 	}
 	free_stack(stack);
